64d8aba3987421d22360b0caa843260888a83ac7
Corey Frenette <corey@narwhunderful.com>
2019-10-31 15:43:48 -0300

    Initial code commit

---
+++ src/main/java/loficomputers/arduinoprogrammer/ArduinoProgrammer.java
@@ -0,0 +1,11 @@
+package loficomputers.arduinoprogrammer;
+
+public final class ArduinoProgrammer {
+  public static void main(final String[] args) {
+    new ArduinoProgrammer();
+  }
+
+  private ArduinoProgrammer() {
+    System.out.println("Hello world");
+  }
+}
